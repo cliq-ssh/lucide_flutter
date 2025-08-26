9e846538cc71985c18d6fe3b43a0c7dadd388daf
Jason Dean Lessenich <jasonlessenich@gmail.com>
2025-08-26 17:04:44 +0200

    Add CHANGELOG, pubspec & README update to upgrade.sh

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # lucide_flutter
 
-A Flutter package for [Lucide](https://lucide.dev/) icons. (`v0.542.0`)
+A Flutter package for [Lucide](https://lucide.dev/) icons. (`v0.541.0`)
 
 ## Usage
 
 Simply use the `LucideIcons` class to access the icons in your Flutter application.
+
 ```dart
+import 'package:lucide_flutter/lucide_flutter.dart';
+
 Icon(LucideIcons.github)
 ```
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -8,9 +8,25 @@ fi
 
 lucide_version=$1
 script_dir=$(dirname "$(readlink -f "$0")")
+changelog_file="$script_dir/../CHANGELOG.md"
 
 "$script_dir/download.sh" "$lucide_version"
 "$script_dir/generate.sh"
+
+# prepend changelog entry
+{
+  echo "## $lucide_version"
+  echo "- Upgraded Lucide icons to version $lucide_version (see full changelog at https://github.com/lucide-icons/lucide/releases/tag/$lucide_version)"
+  echo ""
+  cat "$changelog_file"
+} > "$changelog_file.tmp" && mv "$changelog_file.tmp" "$changelog_file"
+
+# set version in pubspec.yaml
+sed -i -E "s/^version: .*/version: $lucide_version/" "$script_dir/../pubspec.yaml"
+
+# set version in README.md
+sed -i -E "s/v[0-9]+\.[0-9]+\.[0-9]+/v${lucide_version}/g" "$script_dir/../README.md"
+
 
 if [ $? -ne 0 ]; then
   echo "Upgrade process failed." >&2
